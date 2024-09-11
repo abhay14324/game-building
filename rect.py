eec7bdd636506c8cf4f55980fd2bab62bf45111b
abhay14324 <dhirajkumar6580@gmail.com>
2024-09-11 19:54:34 +0530

    game building

---
+++ rect.py
@@ -0,0 +1,15 @@
+import pygame
+pygame.init()
+window=pygame.display.set_mode((400,400))
+done=False
+while not done:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            done=True
+    pygame.draw.rect(window,(0,125,255),pygame.Rect(30,30,60,60))
+    pygame.display.flip()
+            
+
+
+
+
